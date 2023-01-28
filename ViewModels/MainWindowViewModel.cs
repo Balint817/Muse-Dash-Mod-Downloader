cf0f558d97ae31bf9f7f01b918279dfdd95ce717
Balint837 <pecsibalint2@gmail.com>
2023-01-28 16:11:49 +0100

    basically all i have so far

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,59 @@
+using Avalonia;
+using Avalonia.Controls;
+using Muse_Dash_Mod_Downloader.Views;
+using ReactiveUI;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Reactive;
+using System.Runtime.InteropServices;
 using System.Text;
 
 namespace Muse_Dash_Mod_Downloader.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Welcome to Avalonia!";
+        public ReactiveCommand<string, Unit> Run_OpenURL { get; }
+        public MainWindowViewModel()
+        {
+            Run_OpenURL = ReactiveCommand.Create<string>(OpenURL);
+        }
+
+
+        public void OpenURL(string linkToOpen)
+        {
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? linkToOpen : "open",
+                Arguments = RuntimeInformation.IsOSPlatform(OSPlatform.OSX) ? $"-e {linkToOpen}" : "",
+                CreateNoWindow = true,
+                UseShellExecute = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+            });
+        }
+
+        public void Button_FilterAll()
+        {
+            MainWindow._Instance.Selected_ModFilter = 0;
+            MainWindow._Instance.UpdateFilters();
+        }
+
+        public void Button_FilterInstalled()
+        {
+            MainWindow._Instance.Selected_ModFilter = 1;
+            MainWindow._Instance.UpdateFilters();
+        }
+
+        public void Button_FilterEnabled()
+        {
+            MainWindow._Instance.Selected_ModFilter = 2;
+            MainWindow._Instance.UpdateFilters();
+        }
+
+        public void Button_FilterOutdated()
+        {
+            MainWindow._Instance.Selected_ModFilter = 3;
+            MainWindow._Instance.UpdateFilters();
+        }
     }
 }
--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,12 +1,70 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using ReactiveUI;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using System.Reactive;
+using System.Text;
+using static System.Net.WebRequestMethods;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System;
+using System.Configuration;
 
 namespace Muse_Dash_Mod_Downloader.Views
 {
     public partial class MainWindow : Window
     {
+        public static MainWindow _Instance { get; private protected set; } = null;
+        public int Selected_ModFilter { get; set; } = 0;
+        public Version GameVersion { get; private set; } = null;
+
+        public List<WebModInfo> WebModsList { get; private set; }
+
+        private const string ModLinks = "https://raw.githubusercontent.com/MDModsDev/ModLinks/dev/ModLinks.json";
         public MainWindow()
         {
             InitializeComponent();
+            _Instance = this;
+#pragma warning disable SYSLIB0014 // Type or member is obsolete
+            var webClient = new WebClient
+            {
+                Encoding = Encoding.UTF8
+            };
+#pragma warning restore SYSLIB0014 // Type or member is obsolete
+            string data = Encoding.Default.GetString(webClient.DownloadData(ModLinks));
+            webClient.Dispose();
+            WebModsList = JsonSerializer.Deserialize<List<WebModInfo>>(data);
+
+
         }
+
+        public void UpdateFilters()
+        {
+
+        }
+    }
+
+    public class WebModInfo
+    {
+        public string Name { get; set; }
+        public string Version { get; set; }
+        public string Author { get; set; }
+        public string DownloadLink { get; set; }
+        public string HomePage { get; set; }
+        public string[] GameVersion { get; set; }
+        public string Description { get; set; }
+        public string[] DependentMods { get; set; }
+        public string[] DependentLibs { get; set; }
+        public string[] IncompatibleMods { get; set; }
+        public string SHA256 { get; set; }
+    }
+
+    public class LocalModInfo
+    {
+        public string Name { get; set; }
+        public string Version { get; set; }
+        public string SHA256 { get; set; }
     }
 }
